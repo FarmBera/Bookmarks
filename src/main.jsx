46211c3c349ad9257a250417e03799c4f92a278d
FarmBera <101335222+FarmBera@users.noreply.github.com>
2026-02-13 17:48:01 +0900

    Install Vercel Analytics
    Signed-off-by: FarmBera <101335222+FarmBera@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
 import {BrowserRouter} from "react-router-dom";
+import {Analytics} from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <App/>
+            <Analytics/>
         </BrowserRouter>
     </React.StrictMode>
 );
